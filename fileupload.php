0a6ad35c2bc436d78dba0bacf87e8f290e32bec0
Zain <zainulabidin302@gmail.com>
2018-08-16 02:44:21 +0000

    initial commit

---
+++ fileupload.php
@@ -0,0 +1,10 @@
+
+
+<?php
+echo exec('whoami');
+error_reporting(-1);
+ini_set('display_errors', 1);
+    $filepath = $_FILES["file"]["tmp_name"];
+    print_r($filepath);
+  echo  move_uploaded_file($filepath,"images/". $_FILES["file"]["name"]);
+?>
